7f3d1bc3d2f57b65069593bf67042c65ac1f8ca1
Dima <Dmitriy.Slokva@gmail.com>
2018-08-10 15:20:31 +0600

    change to DXCluster backend

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-var express = require("express")
-var ws = require('ws')
-var WEX = require('node-wex')
-var app = express()
+var express = require("express");
+var ws = require('ws');
+var app = express();
 var async = require('async');
-var wexPublic = new WEX();
+var sleep = require('sleep'); 
+var net = require('net');
 
-var wexCoinTickers = ["ltc_usd","btc_usd","zec_usd","bch_usd","dsh_usd", "eth_usd"];
-var bitfinexCoinTickers = ["ltcusd","btcusd","zecusd","bchusd","dshusd", "ethusd"];
+var last50spots = [];
+
+var client = new net.Socket();
+
+client.connect(8000, 'dxc.kfrr.kz', function() {
+  console.log('Connected');
+  once = true;
+});
+
+client.on('data', function(data) {
+  var line = "" + data;
+  
+  if (last50spots.length > 300) last50spots.shift();
+  var dxLine = line.trim();
+  if (dxLine.startsWith("DX"))
+    last50spots.push(dxLine.slice(0, -2));
+
+  console.log(line.trim());
+
+  if (once) {
+    sleep.sleep(1);
+    client.write('UN7ZAF-1\n');
+    once = false;
+  }
+	//client.destroy(); // kill client after server's response
+});
+
+client.on('close', function() {
+	console.log('Connection closed');
+});
 
 
 app.get('/ws', function(req, res) {
@@ -18,67 +46,66 @@ wss = new WebSocketServer({port: 40510})
 
 wss.on('connection', function (ws) {
   ws.on("error", function(ws) {
-      console.log('Clients count: '+wss.clients.size)
+      console.log('Clients count: '+wss.clients.size);
   });
   
   ws.on('message', function (message) {
-    clientMessage = JSON.parse(message)
-    console.log('received: %s', JSON.stringify(clientMessage))
+    clientMessage = JSON.parse(message);
+    console.log('received: %s', JSON.stringify(clientMessage));
 
-    if (clientMessage.request === 'getPublicCoinsData') {  
-      let fullData = JSON.parse('{"public_coin_data":{ "wex_public": [], "bitfinex_public": [] }}');
+    if (clientMessage.request === 'getLast50Spots') {  
+      //console.log(last50spots);
+      
+      let fullData = JSON.parse('{"last_50_spots":[]}');
       let calls = [];
 
-      for (let coinTicker of wexCoinTickers) {
-        calls.push(
-          function(callback) {
-            wexPublic.ticker(coinTicker, function(err, data) {
-              let middleData = JSON.parse(JSON.stringify(data));
-              if (coinTicker == "btc_usd") coinTicker = "Bitcoin";
-              if (coinTicker == "ltc_usd") coinTicker = "Litecoin";
-              if (coinTicker == "zec_usd") coinTicker = "ZCash";
-              if (coinTicker == "bch_usd") coinTicker = "Bitcoin cash";
-              if (coinTicker == "dsh_usd") coinTicker = "Dash coin";
-              if (coinTicker == "eth_usd") coinTicker = "Etherium";
-
-              middleData.coin_pair = coinTicker;
-              fullData.public_coin_data['wex_public'].push(JSON.stringify(middleData));
-              callback();
+      calls.push(function(callback) {
+        let count = 0;
+            last50spots.forEach(function(value) {
+              console.log(value);
+              var rePattern = new RegExp(/DX de ([A-Z0-9\/\-]+):\s+(\d+.\d+)\s+([A-Z0-9\/\-]+)\s+(.*)?(\d{4}Z)/);
+              var arrMatches = value.match(rePattern);
+              // console.log(arrMatches);
+              let spot = JSON.parse('{"spot": {"de": "'+arrMatches[1]+'", "freq": "'+arrMatches[2]+'", "dx": "'+arrMatches[3]+'", "comment": "'+arrMatches[4]+'", "time": "'+arrMatches[5]+'"}}');
+              fullData.last_50_spots.push(spot);
+              count++; 
+              if (count > 49) return;
             });
-          }
-        );
-      }
+
+          callback();
+        }
+      );
 
       async.parallel(calls, function() {
         ws.send(JSON.stringify(fullData));
-        console.log(fullData);
+        //console.log(fullData);
       });
     }
     
-  // setInterval(
-  //   () => {
-  //     var options = {
-  //       year: 'numeric', month: 'long', day: 'numeric',
-  //       weekday: 'long',
-  //       timezone: 'UTC',
-  //       hour: 'numeric', minute: 'numeric', second: 'numeric'
-  //     };
+  setInterval(
+    () => {
+      var options = {
+        year: 'numeric', month: 'long', day: 'numeric',
+        weekday: 'long',
+        timezone: 'UTC',
+        hour: 'numeric', minute: 'numeric', second: 'numeric'
+      };
       
-  //     let date = new Date();
-  //     let dateStr = date.toLocaleString('en-US', options);
+      let date = new Date();
+      let dateStr = date.toLocaleString('en-US', options);
 
-  //     wss.clients.forEach(function each(client) {
-  //       if (client !== ws && client.readyState === ws.OPEN) {
-  //         var tickObj = {} 
-  //         var tick = { dateStr };
-  //         tickObj.tick = tick;
-  //         client.send(JSON.stringify(tickObj));
-  //       }
-  //     });
-  //   },
-  //   10000
-  // )
-  })
+      wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === ws.OPEN) {
+          var tickObj = {} 
+          var tick = { dateStr };
+          tickObj.tick = tick;
+          client.send(JSON.stringify(tickObj));
+        }
+      });
+    },
+    10000
+  );
+  });
 });
 
 app.listen(3000, function(){
